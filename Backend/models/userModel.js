10d455548d7eb87e6e31daa1fca60965f04a0751
Ahmed Mouine Ben Hadj Mohamed <mouinbelhaj@icloud.com>
2025-08-22 12:52:41 +0100

    Refactor user schema definition and enhance password hashing logic

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,27 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: /.+\@.+\..+/,
+      message: "please fill a valid email",
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+      message:
+        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.",
+    },
+    phone: { type: String, required: true },
+    role: { type: String, enum: ["client", "admin"], default: "client" },
+    userImg: { type: String, required: false, default: "client.png" },
+  },
+  { timestamps: true }
+);
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    match: /.+\@.+\..+/,
-    message: "please fill a valid email",
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-    message:
-      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.",
-  },
-  phone: { type: String, required: true },
-  role: { type: String, enum: ["client", "admin"], default: "client" },
-  userImg: { type: String, required: false, default: "client.png" },
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const User = this;
+    User.password = await bcrypt.hash(User.password, salt);
+    User.isActive = false; // Default value for isActive
+
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
